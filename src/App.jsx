c5da8103fa4036ed8a5cc32e6548d9e96bcb9f2b
Ahmed-NourAldin <ahmedmandroc@gmail.com>
2023-06-26 13:38:20 +0300

    finished-landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
 import Navbar from "./scenes/Navbar";
 import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
-import { useEffect, useState } from 'react'
+import MySkills from "./scenes/MySkills";
+import { useEffect, useState } from 'react';
+import LineGradient from "./components/LineGradient";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 
-function App() {
-  const [selectedPage, setSelectedPage] = useState('home')
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
-  const [isTopOfPage, setIsTopOfPage] = useState(true)
+function App ()
+{
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
 
-  useEffect (() => {
-    const handleScroll = () => {
+  useEffect(() =>
+  {
+    const handleScroll = () =>
+    {
       if (window.scrollY === 0) setIsTopOfPage(true);
       if (window.scrollY !== 0) setIsTopOfPage(false);
-    }
-    window.addEventListener("scroll", handleScroll)
+    };
+    window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
+  }, []);
 
   return (
     <div className='text-white app bg-deep-blue flex flex-col flex-wrap'>
-      <Navbar isTopOfPage={isTopOfPage} selectPage={selectedPage} setSelectedPage={setSelectedPage} />
-      {/* Above Medium Screens */}
+      <Navbar isTopOfPage={ isTopOfPage } selectPage={ selectedPage } setSelectedPage={ setSelectedPage } />
+      {/* Above Medium Screens */ }
       <div className='w-5/6 mx-auto md:h-full'>
-        {isAboveMediumScreens && (
-          <DotGroup selectedPage={selectedPage} setSelectedPage={selectedPage} />
-        )}
-        <Landing setSelectedPage={setSelectedPage}/>
+        { isAboveMediumScreens && (
+          <DotGroup selectedPage={ selectedPage } setSelectedPage={ selectedPage } />
+        ) }
+        <Landing setSelectedPage={ setSelectedPage } />
+      </div>
+      <LineGradient />
+      <div className='w-5/6 mx-auto md:h-full'>
+          <MySkills />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const SocialMediaIcons = () => {
+const SocialMediaIcons = () =>
+{
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className='flex justify-center md:justify-start my-10 gap-7'>
 
-export default SocialMediaIcons
+      <a href="https://www.linkedin.com/in/ahmed-esam-front-end/" target='_blank' className="hover:opacity-50 transition duration-500" rel="noreferrer">
+        <img src="https://github.com/ed-roh/react-portfolio/blob/master/public/assets/instagram.png?raw=true" alt="linkedin-link" />
+      </a>
+      <a href="https://www.instagram.com/ahmedenouraldin/" target='_blank' className="hover:opacity-50 transition duration-500" rel="noreferrer">
+        <img src="https://github.com/ed-roh/react-portfolio/blob/master/public/assets/linkedin.png?raw=true" alt="instagram-link" />
+      </a>
+      <a href="https://www.facebook.com/profile.php?id=100057493277186" target='_blank' className="hover:opacity-50 transition duration-500" rel="noreferrer">
+        <img src="https://raw.githubusercontent.com/ed-roh/react-portfolio/master/public/assets/facebook.png" alt="facebook-link" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
